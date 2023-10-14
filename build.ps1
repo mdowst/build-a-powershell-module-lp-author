bd3985196cf87a153420860abc7ec0cc4b5e8ef1
mdowst <7384306+mdowst@users.noreply.github.com>
2023-10-14 07:58:25 -0700

    fix typo

--- build.ps1
+++ build.ps1
@@ -13,7 +13,7 @@ $config = New-PesterConfiguration
 $config.Output.Verbosity = 'Detailed'
 $config.Run.Path = (Join-Path $PSScriptRoot 'Source\Test')
 $config.Run.Throw = $true
-#Invoke-Pester -Configuration $config
+Invoke-Pester -Configuration $config
 
 
 $psd1 = Get-ChildItem .\Build -Filter 'RocinanteGitHub.psd1' -Recurse | Select-Object -Last 1 
