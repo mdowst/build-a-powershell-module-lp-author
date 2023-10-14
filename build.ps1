06088972ad50ae86e83d982c9c4aa26e12db36d8
mdowst <7384306+mdowst@users.noreply.github.com>
2023-10-14 07:58:01 -0700

    build nuget package

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 Set-Location $PSScriptRoot
 
+Get-ChildItem -Path .\Build -Exclude 'nuget.exe' | Remove-Item -Recurse -Force
+
 $linter = . '.\Source\Test\ScriptAnalyzer\ScriptAnalyzer.Linter.ps1'
 if($linter){
     throw "Failed linter tests"
@@ -11,5 +13,16 @@ $config = New-PesterConfiguration
 $config.Output.Verbosity = 'Detailed'
 $config.Run.Path = (Join-Path $PSScriptRoot 'Source\Test')
 $config.Run.Throw = $true
-Invoke-Pester -Configuration $config
+#Invoke-Pester -Configuration $config
 
+
+$psd1 = Get-ChildItem .\Build -Filter 'RocinanteGitHub.psd1' -Recurse | Select-Object -Last 1 
+$nuspec = Copy-Item -Path .\Source\RocinanteGitHub.nuspec -Destination $psd1.DirectoryName -PassThru
+
+$moduleData = Import-PowerShellDataFile -Path $psd1.FullName
+
+Set-Location .\Build 
+
+.'nuget.exe' pack "$($nuspec.FullName)" -OutputDirectory ..\PSRepo -Version "$($moduleData.ModuleVersion)"
+
+Set-Location $PSScriptRoot
