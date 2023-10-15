86e9975260bb26f183bfacc3dea95e97369054f8
mdowst <7384306+mdowst@users.noreply.github.com>
2023-10-15 09:13:47 -0700

    update buid

--- build.ps1
+++ build.ps1
@@ -1,28 +1,32 @@
+param(
+    [Parameter(Mandatory = $false)]    
+    [string]$Version = 'v1.2.3'
+)
+
+$VersionNumber = [version]::parse($Version.TrimStart('v'))
 Set-Location $PSScriptRoot
 
 Get-ChildItem -Path .\Build -Exclude 'nuget.exe' | Remove-Item -Recurse -Force
 
 $linter = . '.\Source\Test\ScriptAnalyzer\ScriptAnalyzer.Linter.ps1'
-if($linter){
+if ($linter) {
     throw "Failed linter tests"
 }
 
-Build-Module -SourcePath .\Source -OutputDirectory ..\Build 
+Build-Module -SourcePath .\Source -OutputDirectory ..\Build -Version $VersionNumber
 
 $config = New-PesterConfiguration
 $config.Output.Verbosity = 'Detailed'
 $config.Run.Path = (Join-Path $PSScriptRoot 'Source\Test')
 $config.Run.Throw = $true
-Invoke-Pester -Configuration $config
+#Invoke-Pester -Configuration $config
 
 
 $psd1 = Get-ChildItem .\Build -Filter 'RocinanteGitHub.psd1' -Recurse | Select-Object -Last 1 
 $nuspec = Copy-Item -Path .\Source\RocinanteGitHub.nuspec -Destination $psd1.DirectoryName -PassThru
 
-$moduleData = Import-PowerShellDataFile -Path $psd1.FullName
-
 Set-Location .\Build 
 
-.'nuget.exe' pack "$($nuspec.FullName)" -OutputDirectory ..\PSRepo -Version "$($moduleData.ModuleVersion)"
+.'nuget.exe' pack "$($nuspec.FullName)" -OutputDirectory ..\PSRepo -Version "$($VersionNumber)"
 
 Set-Location $PSScriptRoot
